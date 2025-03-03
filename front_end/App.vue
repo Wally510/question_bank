a9835630c76b8573fea25ef5724f7a727749281d
Wally510 <106104743+Wally510@users.noreply.github.com>
2025-03-03 15:01:28 +0800

    Add files via upload

---
+++ front_end/App.vue
@@ -0,0 +1,27 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/* .zujuan_button{
+		position: relative;
+		top: 10px; 
+		line-height: 40px; 
+		color: #ffffff;  
+		background-color: #00aaff; 
+		border-radius: 30px; 
+		width: 90%; 
+		height: 40px;
+	} */
+	/*每个页面公共css */
+</style>
